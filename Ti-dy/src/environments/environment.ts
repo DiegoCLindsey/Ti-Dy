6ac68883b9e7aafd633ec4552c2165df7c25b54c
Diego <DiegoClindsey@gmail.com>
2022-11-15 00:33:21 +0100

    Add firebase

--- Ti-dy/src/environments/environment.ts
+++ Ti-dy/src/environments/environment.ts
@@ -1,19 +1,18 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 
 export const environment = {
   production: false,
-    firebaseConfig : {
-    apiKey: "AIzaSyCbFZS_sq7PjJOVsTCNTguXz560a6CUx_I",
-    authDomain: "ti-dy-46680.firebaseapp.com",
-    projectId: "ti-dy-46680",
-    storageBucket: "ti-dy-46680.appspot.com",
-    messagingSenderId: "490690875011",
-    appId: "1:490690875011:web:c1bba0b87341b851f9d5b6",
-    measurementId: "G-BKMDX984LL"
-  },
+
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
@@ -22,4 +21,22 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCbFZS_sq7PjJOVsTCNTguXz560a6CUx_I",
+  authDomain: "ti-dy-46680.firebaseapp.com",
+  projectId: "ti-dy-46680",
+  storageBucket: "ti-dy-46680.appspot.com",
+  messagingSenderId: "490690875011",
+  appId: "1:490690875011:web:c1bba0b87341b851f9d5b6",
+  measurementId: "G-BKMDX984LL"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
